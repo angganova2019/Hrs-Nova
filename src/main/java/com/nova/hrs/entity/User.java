c440c60db7e8180e228a8721d2d6a2a68527f571
angganova2019 <angga24.opa@gmail.com>
2020-07-30 19:54:49 +0700

    create table user

---
+++ src/main/java/com/nova/hrs/entity/User.java
@@ -0,0 +1,12 @@
+package com.nova.hrs.entity;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String token;
+    private boolean email_verified;
+    private LocalDateTime timestamps;
+}
